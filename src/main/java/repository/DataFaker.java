c5d6cc1af3718f26f7af7c0bef00490697005665
Firedog01 <jan.rubacha@onet.pl>
2022-10-14 19:51:41 +0200

    added faker for rent

--- src/main/java/repository/DataFaker.java
+++ src/main/java/repository/DataFaker.java
@@ -4,9 +4,14 @@ import exception.ClientException;
 import model.Address;
 import model.Client;
 import model.EQ.*;
+import model.Rent;
 import model.idType;
+import org.joda.time.DateTime;
+import org.joda.time.Instant;
+import org.joda.time.LocalDateTime;
 
 import javax.xml.crypto.Data;
+import java.time.ZoneOffset;
 import java.util.Random;
 import java.util.random.RandomGenerator;
 
@@ -54,9 +59,34 @@ public class DataFaker {
         return new Microphone(Math.random() * 100, Math.random() * 200,
                 Math.random() * 1000, randStr(10), randStr(8));
     }
+
     public static Trivet getTrivet() {
         return new Trivet(Math.random() * 100, Math.random() * 200,
                 Math.random() * 1000, randStr(10), Math.random() * 10);
+    }
+
+    public static Rent getRent(Equipment e, Client c, Address a) {
+        if(e == null) {
+            e = getCamera();
+        }
+        if(c == null) {
+            c = getClient();
+        }
+        if(a == null) {
+            a = getAddress();
+        }
+        LocalDateTime nowLocal = LocalDateTime.now();
+        DateTime now = nowLocal.toDateTime();
+        long sinceEpoch = now.getMillis();
+        long beginUnix = sinceEpoch + (long) (Math.random() * 10000000);
+        long endUnix = beginUnix + (long) (Math.random() * 10000000);
+        LocalDateTime begin = Instant.ofEpochMilli(beginUnix).toDateTime().toLocalDateTime();
+        LocalDateTime end = Instant.ofEpochMilli(endUnix).toDateTime().toLocalDateTime();
+        return new Rent(begin, end, e, c, a);
+    }
+
+    public static Rent getRent() {
+        return getRent(null, null, null);
     }
 
 
