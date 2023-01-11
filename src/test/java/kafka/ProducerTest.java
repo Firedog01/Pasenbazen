75031d87757c42b8827f729de4f5b1af56ed6241
AverageCoffeeEnjoyer <bartlomiej.czajkowski20@gmail.com>
2023-01-11 10:08:09 +0100

    Working kafka, now its time to reshape whole app

--- src/test/java/kafka/ProducerTest.java
+++ src/test/java/kafka/ProducerTest.java
@@ -25,9 +25,9 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.common.serialization.UUIDDeserializer;
 import org.apache.kafka.common.serialization.UUIDSerializer;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.text.MessageFormat;
@@ -40,11 +40,13 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-@Disabled //Not working for now, error in producer method
 public class ProducerTest {
 
+    static String testProd;
+    static String testCons;
     static KafkaProducer<UUID, String> producer;
-    static KafkaConsumer consumer;
+    static KafkaConsumer<UUID, String> consumer1;
+    static KafkaConsumer<UUID, String> consumer2;
     static List<KafkaConsumer<UUID, String>> consumerGroup = new ArrayList<>();
 
     static LocalDateTime t0 = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
@@ -68,9 +70,8 @@ public class ProducerTest {
         producerConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, UUIDSerializer.class.getName());
         producerConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         producerConfig.put(ProducerConfig.CLIENT_ID_CONFIG, "local");
-        producerConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9191,kafka2:9291,kafka3:9391");
+        producerConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9192,kafka2:9292,kafka3:9392");
         producerConfig.put(ProducerConfig.ACKS_CONFIG, "all");
-        producerConfig.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "0fdf73dd-bd48-48f1-bb1b-625ba29f6c04");
         producerConfig.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
         producer = new KafkaProducer<UUID, String>(producerConfig);
 
@@ -79,12 +80,20 @@ public class ProducerTest {
         consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, Topics.CONSUMER_GROUP_NAME);//dynamiczny przydział
 //consumerConfig.put(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "clientconsumer");//statyczny przydział
-        consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9191,kafka2:9291,kafka3:9391");
-        consumer = new KafkaConsumer(consumerConfig);
-        consumer.subscribe(List.of(Topics.CLIENT_TOPIC));
+        consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9192,kafka2:9292,kafka3:9392");
+        consumer1 = new KafkaConsumer<UUID, String>(consumerConfig);
+//        consumer2 = new KafkaConsumer<UUID, String>(consumerConfig);
+        consumer1.subscribe(List.of(Topics.CLIENT_TOPIC));
+//        consumer2.subscribe(List.of(Topics.CLIENT_TOPIC));
+        consumerGroup.add(consumer1);
+//        consumerGroup.add(consumer2);
     }
 
-
+    @AfterAll
+    public static void afterAll() {
+        System.out.println(testProd);
+        System.out.println(testCons);
+    }
 
     @Test
     public void createTopic() throws InterruptedException {
@@ -115,6 +124,64 @@ public class ProducerTest {
         }
     }
 
+
+    @Test
+    public void sendMessages() throws ExecutionException, InterruptedException, JsonProcessingException {
+//        initProducer();
+        RentMgd rentMgd = DataFakerMgd.getRentMgd(t0, t1);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        ProducerRecord<UUID, String> rentRecord = new ProducerRecord<>(Topics.CLIENT_TOPIC,
+                rentMgd.getEntityId().getUuid(), objectMapper.writeValueAsString(rentMgd));
+        Future<RecordMetadata> sent = producer.send(rentRecord);
+        RecordMetadata recordMetadata = sent.get();
+        testProd = recordMetadata.toString();
+    }
+
+
+
+    @Test
+    public void consumeTopicsByGroup() throws InterruptedException, ExecutionException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        for (KafkaConsumer<UUID, String> consumer : consumerGroup) {
+            executorService.execute(() -> consume(consumer));
+        }
+        Thread.sleep(10000);
+        for (KafkaConsumer<UUID, String> consumer : consumerGroup) {
+            consumer.wakeup();
+        }
+        executorService.shutdown();
+        deleteConsumerGroup();
+    }
+
+    private void consume(KafkaConsumer<UUID, String> consumer) {
+        try {
+            consumer.poll(Duration.ZERO);
+            Set<TopicPartition> consumerAssignment = consumer.assignment();
+            System.out.println(consumer.groupMetadata().memberId() + " " + consumerAssignment);
+            consumer.seekToBeginning(consumerAssignment);
+            Duration timeout = Duration.of(100, ChronoUnit.MILLIS);
+            MessageFormat formatter = new MessageFormat("Konsument {5},Temat {0}, partycja {1}, offset {2, number, integer}, klucz {3}, wartość {4}");
+            while (true) {
+                ConsumerRecords<UUID, String> records;
+                records = consumer.poll(timeout);
+                for (ConsumerRecord<UUID, String> record : records) {
+                    testCons = record.value();
+                    String result = formatter.format(new Object[]{
+                            record.topic(),
+                            record.partition(),
+                            record.offset(),
+                            record.key(),
+                            record.value(),
+                            consumer.groupMetadata().memberId()
+                    });
+                }
+            }
+        } catch (WakeupException we) {
+            System.out.println("Job Finished");
+        }
+    }
+
     private void deleteConsumerGroup() throws ExecutionException, InterruptedException {
         Properties properties = new Properties();
         properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9192,kafka2:9292,kafka3:9392");
@@ -127,110 +194,6 @@ public class ProducerTest {
                 System.out.println(consumerGroupDescription);
             }
             admin.deleteConsumerGroups(List.of(Topics.CONSUMER_GROUP_NAME));
-        }
-    }
-
-    @Test
-    public void sendMessages() throws ExecutionException, InterruptedException, JsonProcessingException {
-//        initProducer();
-        RentMgd rentMgd = DataFakerMgd.getRentMgd(t0, t1);
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        ProducerRecord<UUID, String> rentRecord = new ProducerRecord<>(Topics.CLIENT_TOPIC,
-                rentMgd.getEntityId().getUuid(), "objectMapper.writeValueAsString(rentMgd)");
-        System.out.println(rentRecord);
-        Future<RecordMetadata> sent = producer.send(rentRecord); //FIXME Error
-        // java.util.concurrent.ExecutionException: org.apache.kafka.common.errors.
-        // UnsupportedVersionException: The broker does not support METADATA
-//        producer.send(rentRecord, this::onCompletion);
-        RecordMetadata recordMetadata = sent.get();
-//        System.out.println(objectMapper.writeValueAsString(rentMgd));
-        System.out.println(recordMetadata.toString());
-
-    }
-
-    private void receiveClients() {
-        int messagesReceived = 0;
-        Map<Integer, Long> offsets = new HashMap<>();
-        Duration timeout = Duration.of(100, ChronoUnit.MILLIS);
-        MessageFormat formatter = new MessageFormat("Temat {0}, partycja {1}, offset {2, number, integer}, klucz {3}, wartość {4}");
-        while (messagesReceived < 100) { //while true?
-            ConsumerRecords<UUID, String> records = consumer.poll(timeout);
-            for (ConsumerRecord<UUID, String> record : records) {
-                String result = formatter.format(new Object[]{record.topic(), record.partition(),
-                        record.offset(), record.key(), record.value()});
-                System.out.println(result);
-                offsets.put(record.partition(), record.offset());
-                messagesReceived++;
-            }
-        }
-        System.out.println(offsets);
-        consumer.commitAsync();
-    } //FIXME DISABLE AUTOCOMMIT? BUT WHERE
-
-
-    public void receiveFromBeginning() { //KOD NIE PRODUKCYJNY, CZYLI NIE NADAJE SIĘ?
-        consumer.poll(0);
-        Set<TopicPartition> consumerAssignment = consumer.assignment();
-        System.out.println(consumerAssignment);
-        consumer.seekToBeginning(consumerAssignment);
-        Duration timeout = Duration.of(100, ChronoUnit.MILLIS);
-        MessageFormat formatter = new MessageFormat("Temat {0}, partycja {1}, offset {2, number, integer}, klucz {3}, wartość {4}");
-        Set<TopicPartition> unfinishedPartitions = new HashSet<>(consumerAssignment);
-        while (!unfinishedPartitions.isEmpty()) {
-            ConsumerRecords<UUID, String> records = consumer.poll(timeout);
-            for (ConsumerRecord<UUID, String> record : records) {
-                String result = formatter.format(new Object[]{record.topic(), record.partition(), record.offset(), record.key(),
-                        record.value()});
-                System.out.println(result);
-                for (TopicPartition partition : consumerAssignment) {
-                    long position = consumer.position(partition, timeout);
-                    if (partition.partition() == record.partition() && record.offset() == position - 1) {
-                        unfinishedPartitions.remove(partition);
-                    }
-                }
-            }
-        }
-    }
-
-
-
-    public void consumeTopicsByGroup() throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
-        for (KafkaConsumer<UUID, String> consumer : consumerGroup) {
-            executorService.execute(() -> consume(consumer));
-        }
-        Thread.sleep(10000);
-        for (KafkaConsumer<UUID, String> consumer : consumerGroup) {
-            consumer.wakeup();
-        }
-        executorService.shutdown();
-    }
-
-    private void consume(KafkaConsumer<UUID, String> consumer) {
-        try {
-            consumer.poll(0);
-            Set<TopicPartition> consumerAssignment = consumer.assignment();
-            System.out.println(consumer.groupMetadata().memberId() + " " + consumerAssignment);
-            consumer.seekToBeginning(consumerAssignment);
-            Duration timeout = Duration.of(100, ChronoUnit.MILLIS);
-            MessageFormat formatter = new MessageFormat("Konsument {5},Temat {0}, partycja {1}, offset {2, number, integer}, klucz {3}, wartość {4}");
-            while (true) {
-                ConsumerRecords<UUID, String> records = consumer.poll(timeout);
-                for (ConsumerRecord<UUID, String> record : records) {
-                    String result = formatter.format(new Object[]{
-                            record.topic(),
-                            record.partition(),
-                            record.offset(),
-                            record.key(),
-                            record.value(),
-                            consumer.groupMetadata().memberId()
-                    });
-                    System.out.println(result);
-                }
-            }
-        } catch (WakeupException we) {
-            System.out.println("Job Finished");
         }
     }
 }
--- src/test/java/kafka/Topics.java
+++ src/test/java/kafka/Topics.java
@@ -1,7 +1,7 @@
 package kafka;
 
 public class Topics {
-    public static final String CLIENT_TOPIC = "RENTS";
+    public static final String CLIENT_TOPIC = "RENTS_TEST";
     public static final String CONSUMER_GROUP_NAME = "CLIENTS";
 
 }
