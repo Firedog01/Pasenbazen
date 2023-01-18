f839a31d158896b230e1ae94d0aa6a5492505568
AverageCoffeeEnjoyer <bartlomiej.czajkowski20@gmail.com>
2023-01-18 05:46:37 +0100

    Working kafka for sure

--- src/test/java/kafka/ProducerTest.java
+++ src/test/java/kafka/ProducerTest.java
@@ -15,10 +15,8 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
-import org.apache.kafka.common.KafkaException;
 import org.apache.kafka.common.KafkaFuture;
 import org.apache.kafka.common.TopicPartition;
-import org.apache.kafka.common.errors.ProducerFencedException;
 import org.apache.kafka.common.errors.TopicExistsException;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -33,10 +31,7 @@ import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 import java.util.logging.Logger;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -44,8 +39,6 @@ public class ProducerTest {
 
     static Logger logger = Logger.getLogger(ProducerTest.class.getName());
 
-    static String testProd;
-    static String testCons;
     static KafkaProducer<UUID, String> producer;
     static KafkaConsumer<UUID, String> consumer1;
     static KafkaConsumer<UUID, String> consumer2;
@@ -54,11 +47,9 @@ public class ProducerTest {
     static LocalDateTime t0 = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
     static LocalDateTime t1 = t0.plusDays('1');
 
-    static AddressMgd address1 = DataFakerMgd.getAddressMgd();
-    static AddressMgd address2 = DataFakerMgd.getAddressMgd();
-    static AddressMgd address3 = DataFakerMgd.getAddressMgd();
+    static RentRepository repository;
 
-    static RentRepository repository = new RentRepository();
+
     static ObjectMapper objectMapper = new ObjectMapper();
 
     @BeforeAll
@@ -68,7 +59,7 @@ public class ProducerTest {
         producerConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         producerConfig.put(ProducerConfig.CLIENT_ID_CONFIG, "local");
         producerConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9192,kafka2:9292,kafka3:9392");
-//        producerConfig.put(ProducerConfig.ACKS_CONFIG, "all");
+        producerConfig.put(ProducerConfig.ACKS_CONFIG, "all");
         producerConfig.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
 //        producerConfig.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, "4da972e7-ae0a-4e28-b133-1321007663a4");
         producer = new KafkaProducer<UUID, String>(producerConfig);
@@ -77,6 +68,7 @@ public class ProducerTest {
         consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, UUIDDeserializer.class.getName());
         consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, Topics.CONSUMER_GROUP_NAME);//dynamiczny przydział
+        consumerConfig.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
 //consumerConfig.put(ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "clientconsumer");//statyczny przydział
 //        consumerConfig.put(ConsumerConfig.ISOLATION_LEVEL_CONFIG, "read_committed");
         consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9192,kafka2:9292,kafka3:9392");
@@ -88,23 +80,24 @@ public class ProducerTest {
         consumerGroup.add(consumer2);
         objectMapper.registerModule(new JavaTimeModule());
 
-    }
-
-    @AfterAll
-    public static void after() {
-        List<RentMgd> mgdList = repository.getAll();
-
-        for (RentMgd rentMgd :
-                mgdList) {
-            logger.warning("Rent: " + rentMgd.toString());
-//            repository.remove(rentMgd);
-        }
-
-//        logger.warning(repository.getAll().toString());
+        logger.warning("testBeforeAll");
     }
 
     @Test
     @Order(1)
+    public void dropMongo() {
+        repository = new RentRepository();
+        List<RentMgd> mgdList = repository.getAll();
+
+        for (RentMgd rentMgd :
+                mgdList) {
+            repository.remove(rentMgd);
+        }
+
+    }
+
+    @Test
+    @Order(2)
     public void createTopic() throws InterruptedException {
         Properties properties = new Properties();
         properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:9192,kafka2:9292,kafka3:9392");
@@ -125,78 +118,75 @@ public class ProducerTest {
         }
     }
 
-    private void onCompletion(RecordMetadata data, Exception exception) {
-        if (exception == null) {
-            logger.warning(String.valueOf(data.offset()));
-        } else {
-            logger.warning(exception.getMessage());
-        }
-    }
-
-
     @Test
-    @Order(2)
+    @Order(3)
     public void sendMessages() throws ExecutionException, InterruptedException, JsonProcessingException {
-//        producer.initTransactions();
         try {
             for (int i = 0; i < 5; i++) {
                 RentMgd rentMgd = DataFakerMgd.getRentMgd(t0, t1);
                 logger.warning(rentMgd.toString());
                 ProducerRecord<UUID, String> rentRecord = new ProducerRecord<>(Topics.CLIENT_TOPIC,
                         rentMgd.getEntityId().getUuid(), objectMapper.writeValueAsString(rentMgd));
-                producer.send(rentRecord);
+                Future<RecordMetadata> sent = producer.send(rentRecord);
+                RecordMetadata recordMetadata = sent.get();
+                logger.warning(recordMetadata.toString());
             }
-//            producer.commitTransaction();
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
-        } catch (ProducerFencedException e) {
-//            producer.close();
-        } catch (KafkaException ke) {
-//            producer.abortTransaction();
         }
-//        RecordMetadata recordMetadata = sent.get();
-//        testProd = recordMetadata.toString();
     }
 
-
     @Test
-    @Order(3)
+    @Order(4)
     public void consumeTopicsByGroup() throws InterruptedException, ExecutionException {
         ExecutorService executorService = Executors.newFixedThreadPool(2);
         logger.warning("consumer group: " + consumerGroup);
+
         for (KafkaConsumer<UUID, String> consumer : consumerGroup) {
-            logger.warning(consumer.toString());
             executorService.execute(() -> consume(consumer));
         }
+
+
         Thread.sleep(10000);
         for (KafkaConsumer<UUID, String> consumer : consumerGroup) {
             consumer.wakeup();
         }
+        executorService.awaitTermination(10, TimeUnit.SECONDS);
         executorService.shutdown();
         deleteConsumerGroup();
     }
 
+    @Test
+    @Order(5)
+    public void printDatabase() {
+        List<RentMgd> mgdList = repository.getAll();
+
+        if (mgdList.isEmpty()) {
+            logger.warning("MONGO EMPTY");
+        }
+        for (RentMgd rentMgd :
+                mgdList) {
+            logger.warning("MONGO Database: " + rentMgd);
+        }
+    }
+
     private void consume(KafkaConsumer<UUID, String> consumer) {
-//        try {
         consumer.poll(Duration.ZERO);
         Set<TopicPartition> consumerAssignment = consumer.assignment();
         logger.warning("here consumer");
         consumer.seekToBeginning(consumerAssignment);
         Duration timeout = Duration.of(100, ChronoUnit.MILLIS);
         MessageFormat formatter = new MessageFormat("Konsument {5},Temat {0}, partycja {1}, offset {2, number, integer}, klucz {3}, wartość {4}");
-
+        List<String> values = new ArrayList<>();
         try {
             while (true) {
-//                logger.warning(consumer.toString());
                 ConsumerRecords<UUID, String> records;
                 records = consumer.poll(timeout);
-//                logger.warning(consumer.toString() + "    " + String.valueOf(records.count()));
-
 
                 for (ConsumerRecord<UUID, String> record : records) {
                     String value = record.value();
                     logger.warning("RECORD VALUE" + value);
-                    saveToDB(value);
+                    values.add(value);
                     String result = formatter.format(new Object[]{
                             record.topic(),
                             record.partition(),
@@ -211,10 +201,16 @@ public class ProducerTest {
             }
         } catch (WakeupException we) {
             logger.warning("Job Finished");
+
+            RentRepository repository = new RentRepository();
+            for (String value :
+                    values) {
+                saveToDB(repository, value);
+            }
         }
     }
 
-    private void saveToDB(String value) {
+    private void saveToDB(RentRepository repository, String value) {
         try (RentMgd rent = objectMapper.readValue(value, RentMgd.class)) {
             repository.add(rent);
         } catch (JsonProcessingException e) {
